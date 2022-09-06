c2ee0a7ac8f0ce565ce422af6413b094e22ff8f7
sol <sadlthf@icloud.com>
2022-09-06 19:34:07 +0900

    #9 - userController Implementation
    * `ResponseEntity<>` 를 이용함으로써 HttpStatus도 정할 수 있다.

---
+++ server-product-management/src/main/java/com/sha/serverproductmanagement/controller/UserController.java
@@ -0,0 +1,59 @@
+package com.sha.serverproductmanagement.controller;
+
+import com.sha.serverproductmanagement.model.Role;
+import com.sha.serverproductmanagement.model.Transaction;
+import com.sha.serverproductmanagement.model.User;
+import com.sha.serverproductmanagement.service.ProductService;
+import com.sha.serverproductmanagement.service.TransactionService;
+import com.sha.serverproductmanagement.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.security.Principal;
+import java.time.LocalDateTime;
+
+@RestController
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+    private final ProductService productService;
+    private final TransactionService transactionService;
+
+    @PostMapping("/api/user/registration")
+    public ResponseEntity<?> register(@RequestBody User user) {
+        if(userService.findByUsername(user.getUsername()) != null){
+            return new ResponseEntity<>(HttpStatus.CONFLICT);
+        }
+        // default role
+        user.setRole(Role.USER);
+        return new ResponseEntity<>(userService.saveUser(user), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/api/user/login")
+    public ResponseEntity<?> getUSer(Principal principal) {
+//        Principal principal = (new HttpServletRequest()).getUserPrincipal();
+        if (principal == null || principal.getName() == null) {
+            return ResponseEntity.ok(principal);
+        }
+        return new ResponseEntity<>(userService.findByUsername(principal.getName()), HttpStatus.OK);
+    }
+
+    @PostMapping("/api/user/purchase")
+    public ResponseEntity<?> purchaseProduct(@RequestBody Transaction transaction) {
+        transaction.setPurchaseDate(LocalDateTime.now());
+        return new ResponseEntity<>(transactionService.saveTransaction(transaction), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/api/user/products")
+    public ResponseEntity<?> getAllProducts() {
+        return new ResponseEntity<>(productService.findAllProducts(), HttpStatus.OK);
+    }
+
+}
