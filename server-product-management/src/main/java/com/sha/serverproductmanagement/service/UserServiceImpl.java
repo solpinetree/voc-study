03644f66eafaf0d4d06bf5999707a7ce76d27f8d
sol <sadlthf@icloud.com>
2022-09-06 18:58:38 +0900

    #7 - Service Implementation for User, Product, Transaction
    * saveUser 할 때에, passwordEncoder 값을 넣는 방법으로 `setter`를 사용한다. 
    // TODO : setPassword 다른 방법으로 바꾸기

---
+++ server-product-management/src/main/java/com/sha/serverproductmanagement/service/UserServiceImpl.java
@@ -0,0 +1,49 @@
+package com.sha.serverproductmanagement.service;
+
+import com.sha.serverproductmanagement.model.User;
+import com.sha.serverproductmanagement.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public User saveUser(User user) {
+        // TODO: setPassword 변경
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return userRepository.save(user);
+    }
+
+    @Override
+    public User updateUser(User user) {
+        return userRepository.save(user);
+    }
+
+    @Override
+    public void deleteUser(Long userId) {
+        userRepository.deleteById(userId);
+    }
+
+    @Override
+    public User findByUsername(String username) {
+        return userRepository.findByUsername(username).orElse(null);
+    }
+
+    @Override
+    public List<User> findAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public Long numberOfUsers() {
+        return userRepository.count();
+    }
+}
