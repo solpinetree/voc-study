6ec89f4369d0ac4ebe12dfa06cd1b4a3770a8e24
sol <sadlthf@icloud.com>
2022-09-12 20:45:15 +0900

    #9 - adminController Implementation
    * `Long` type isn't suitable for rest api, so created `StringResponse` class

--- server-product-management/src/main/java/com/sha/serverproductmanagement/controller/UserController.java
+++ server-product-management/src/main/java/com/sha/serverproductmanagement/controller/UserController.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletRequest;
 import java.security.Principal;
 import java.time.LocalDateTime;
 
